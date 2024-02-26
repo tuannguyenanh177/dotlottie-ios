46f5fa67ea37726a40f58471c1b5f2b3d48d12f0
Tuan Nguyen <tuannguyenanh177@gmail.com>
2024-02-26 10:18:48 +0700

    update packpage

--- Package.swift
+++ Package.swift
@@ -1,30 +1,33 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "dotLottie",
-    platforms: [
-        .iOS(.v11),
-        .macOS(.v10_12),
-        .tvOS(.v9)
-    ],
-    products: [
-        .library(
-            name: "dotLottie",
-            targets: ["dotLottie"]),
-    ],
-    dependencies: [
-        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-spm", from: "4.1.3"),
-        .package(name: "dotLottieLoader", url: "https://github.com/dotLottie/dotLottieLoader", from: "0.1.8"),
-    ],
-    targets: [
-        .target(
-            name: "dotLottie",
-            dependencies: ["Lottie", "dotLottieLoader"]),
-        .testTarget(
-            name: "dotLottieTests",
-            dependencies: ["dotLottie"]),
-    ]
+  name: "dotLottie",
+  platforms: [
+    .iOS(.v11),
+    .macOS(.v10_12),
+    .tvOS(.v9)
+  ],
+  products: [
+    .library(
+      name: "dotLottie",
+      targets: ["dotLottie"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/lottie-spm", from: "4.4.1"),
+    .package(url: "https://github.com/dotLottie/dotLottieLoader", exact: "0.2.0"),
+  ],
+  targets: [
+    .target(
+      name: "dotLottie",
+      dependencies: [
+        .product(name: "Lottie", package: "lottie-spm"),
+        .byName(name: "dotLottieLoader"),
+      ]),
+    .testTarget(
+      name: "dotLottieTests",
+      dependencies: ["dotLottie"]),
+  ]
 )
